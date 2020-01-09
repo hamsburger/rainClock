f3d29e08feb240634d920237f613f8f2c98b0f7d
harris222 <harris222@users.noreply.github.com>
2020-01-09 15:27:44 -0500

    more commits

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
+/* Maybe we should add a slidey bar that allows users to control the speed of rain */
+
+// Note to self: no truncation is allowed in javascript
 const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
 let date = new Date();
-console.log(date);
 let hours = date.getHours();
 let minutes = date.getMinutes();
-let minuteCounter = 0;
 let seconds = date.getSeconds();
 let currWindowWidth = window.innerWidth;
 let currWindowHeight = window.innerHeight;
-let removeCounter = 0;
+let minuteCounter = 0;
+let removeCounter = 0; // notifies program to remove rain that goes outside of client window bounds.
+let rateOfRain = 2; // 2 is the default value for the rate of rain. Users can feel free to change this value using a slidey bar
 
-// Generate audio
+/* Create media files that connect to the Web Audio API:
+https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_API.
+This is done so that I can control audio in javascript.
+*/
 const audioContext = new AudioContext();
 const tick = document.querySelector('.tick');
 const track1 = audioContext.createMediaElementSource(tick);
@@ -30,9 +36,14 @@ const rain = document.querySelector('.rain');
 const track4 = audioContext.createMediaElementSource(rain);
 track4.connect(audioContext.destination);
 
-// Use smoothfade?
-// No truncation in javascript
+
+/* Function : runClock()
+*  Description: Move clock hands according to current time and update bounds of rainfall when
+client resizes screen dimensions. */
 function runClock(){
+
+  /* Note to self: Although I could use a removeCounter to keep track of when I should redraw the rain to fit within the bounds of
+  current client screen, I could've also used window.resize. */
   if (currWindowHeight - window.innerHeight != 0 || currWindowWidth - window.innerWidth != 0){
     removeCounter += 1;
     playRain();
@@ -46,13 +57,15 @@ function runClock(){
   if (rain.paused){
     rain.play();
   }
-  // Mistakes: Don't do minutes = minutes + Math.floor(seconds/60). If you do this, the hands will eventually synchronize into one movement,
-  // and eventually, the minute hand will turn so fast that you won't be even able to see it rotate.
+
+
+  // An anime character squeals every 1 minute.
   if (seconds % 60 == 0){
     minutes += 1;
     kanna.play();
   }
 
+  // An anime character squeals every 10 minutes
   if (minutes % 10 == 0){
     maid.play();
   } else if (minutes % 60 == 0){
@@ -65,21 +78,36 @@ function runClock(){
   SECONDHAND.style.transform = "rotate(" + seconds*(360/60) + "deg)";
 }
 
+/* Function: playRain
+* Description: Removes rain if there is rain outside the bounds of the client screen (or when removeCounter == 1);
+* Otherwise, this function simply produces rain within the bounds of the screen, which are represented
+by gradient lines (See .rain{} in style.css)
+*/
+
+/* Question of the Day: What's the slowest rate of rain I can produce so that
+* the rain would immediately regenerate after it finishes its animation loop?
+
+How do I derive a formula for rate of rain wrt to when all the rain stops generating? */
 function playRain(){
-  for(i = 0; i < 858; i++){
+
+  for(i = 0; i < 429; i++){
+    let timeOfRainfall = Math.random()*(rateOfRain) + 2; // Time of rainfall varies from 1s to 3s
     if (removeCounter > 0){
         $('#rain' + i).remove();
     }
-    
+
     dropLeft = Math.random() * (window.innerWidth);
-    dropTop = Math.random() * (window.innerHeight) - window.innerHeight/2;
+    dropTop = Math.random() * (window.innerHeight) - window.innerHeight/4;
+
     $('.rainGenerator').append("<div class='rain' id='rain" + i + "'></div>");
+
     $("#rain" + i).css('left', dropLeft);
     $("#rain" + i).css('top', dropTop);
+    $("#rain" + i).css('animation', 'rainFall infinite linear ' + timeOfRainfall + 's');
   } // for
 }
 
 window.onload = function(){
   playRain();
-  var interval = setInterval(runClock, 1000);
+  var interval = setInterval(runClock, 1000); // Clock is updated every second
 };
